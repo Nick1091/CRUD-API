5c07c97c3816aff5b64d77e90b53e150d6258ec3
Nick1091 <nick10.91@mail.ru>
2022-06-19 23:11:22 +0300

    feat: refactor multi.ts file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 import  { v4 as uuidv4 } from 'uuid'
+import { pid } from 'process';
 import { IUser, IUsersWithId } from '../types'
 
 class UsersService {
@@ -9,21 +10,29 @@ class UsersService {
   }
 
   async GetUsers(): Promise<IUser[]> {
+    const usr = this.users
+    process.send?.({ usr, pid })
     return this.users;
   }
 
   async CreateUser(user: IUsersWithId): Promise<IUser> {
     const newUser = {id: uuidv4(), ...user};
     this.users.push(newUser);
+    const usr = this.users
+    process.send?.({ usr, pid })
     return newUser
   }
 
   async GetUserById(id: string): Promise<IUser | undefined> {
+    const usr = this.users
+    process.send?.({ usr, pid })
     return this.users.find((user: IUser) => user.id === id);
   }
 
   async DeleteUser(id: string): Promise<void> {
     this.users = this.users.filter((user) => user.id !== id)
+    const usr = this.users
+    process.send?.({ usr, pid })
   }
 
   async UpdateUser( user: IUser): Promise<IUser> {
@@ -31,6 +40,8 @@ class UsersService {
       us.id === user.id
     })
     this.users.splice(ind, 1 , user)
+    const usr = this.users
+    process.send?.({ usr, pid })
     return user;
   }
 }
--- src/multi.ts
+++ src/multi.ts
@@ -1,34 +1,35 @@
 import { server } from './server';
-import cluster from 'cluster';
+import cluster, {Worker} from 'cluster';
 import { cpus } from 'os';
 import process from 'process';
-import './models';
+import UsersService from './models';
+import { IUser } from 'types';
+
+process.on('message', (msg: IUser[]) => {
+  UsersService.users = msg
+})
 
 const numCPUs = cpus().length;
 if (cluster.isPrimary) {
-  console.log(`Master process started`);
+  console.log(`Master process ${process.pid}started`);
+  const workers: Worker[] = [];
 
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-
-  for (const id in cluster.workers) {
-    cluster.workers[id]?.on('message', async (msg) => {
-      const {workerId, command, data, id: userId} = msg
-      for (const id in cluster.workers) {
-          if(id == workerId) {
-            cluster.workers[id]?.send({command, data, id: userId})
-          }
-      }
-    });
+    const worker = cluster.fork();
+    workers.push(worker);
+    
+    worker.on('message', ({ pid, usr }) => {
+      workers.forEach((wrk) => {
+        !wrk.isDead() && wrk.process.pid !== pid && wrk.send(usr)
+      })
+    })
   }
 } else  {
-    const worker = cluster.worker;
+  const worker = cluster.worker;
     if(worker){
+      server.addListener('request', (req, res) => {
+        console.log(`Worker ${worker.id} - ${process.pid}: ${res.statusCode} ${req.method} - ${req.url}`);
+      })
       server.listen(4000, () => {
         console.log(`Server is running on port ${4000}`)
       });
